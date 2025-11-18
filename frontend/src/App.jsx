bc488763def19a4d5917a7669de35e0399801da9
Yihong Li <154848136+yihongli-04@users.noreply.github.com>
2025-11-18 12:15:58 -0800

    set up Material UI library and basic component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,45 @@
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
+import { useState } from 'react';
 
 function App() {
+  const [model, setModel] = useState(1);
+
+  const handleChange = (event) => {
+    setModel(event.target.value);
+  };
 
   return (
-    <div className='text-6xl text-center'>ecs171 project</div>
+    <div className='flex flex-col justify-center items-center'>
+      <div className='header w-[50rem]'>
+        <div className='text-3xl text-left'>ECS171: News Market Predictor</div>
+        <div className='text-sm'>Group 4: Honore Alexander, Owen Holt, Pranavi Khanna,  Dylan Lim, Yihong Li, Ethan Lee, Dan Firstenberg, Hyeongseung Nam, Oscar Pineda, Kevin Zhang, Zachary Chan, Vicente Aguayo
+        </div>
+      </div>
+
+      <div className='Model Selector mt-[5rem]'>
+        <Box sx={{ minWidth: 120 }}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Model</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={model}
+              label="Model"
+              onChange={handleChange}
+            >
+              <MenuItem value={1}>Model 1</MenuItem>
+              <MenuItem value={2}>Model 2</MenuItem>
+              <MenuItem value={3}>Model 3</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+      </div>
+    </div>
   )
 };
 
